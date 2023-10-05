be747496ea5a6c3d72e6ebb49d89b9f3ee700085
leesoosu <144989207+leesoosu@users.noreply.github.com>
2023-10-05 16:17:08 +0900

    4

--- main.c
+++ main.c
@@ -3,16 +3,14 @@
 
 int main(int argc, char *argv[])
 {
-  int c, count=0;
+  int a, i, sum = 0;
   
-  printf("input a string : ");
+  printf("input a number : ");
+  scanf("%d", &a);
   
-  while((c=getchar())!='\n')
-  {
-      if(c>='0' && c<='9')
-         count++;         
-  }
-  printf("the number of digits is %d\n", count);
+  for(i=1; i<=a; i++)
+      sum = sum+i;
+  printf("The result is %d\n", sum);
   
   system("PAUSE");	
   return 0;
